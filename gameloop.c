74dfb184a99e85d8f8fc73418f600f2d4b8c5c4f
DaSmasher1674 <samkostas1674@gmail.com>
2022-12-01 12:46:24 +0200

    new branch new error

---
+++ gameloop.c
@@ -0,0 +1,6 @@
+#include "structures.c"
+
+void gameloop(struct Card *pDeck) {
+    struct Card *top = pDeck + 51;
+    printf("/n%p", top);
+}
--- main.c
+++ main.c
@@ -3,18 +3,16 @@
 
 #include "debug.c"
 #include "deck.c"
+#include "gameloop.c"
 
 /* Κανόνες παιχνιδιού: WIP*/
 
 int main() {
     struct Card deck[52];      //Κάθε "Card" έχει μία τιμή "int value" (1-10 για τους αριθμούς ενώ οι φιγούρες στο εσωτερικά του προγράμματος συμβολίζονται J=20, Q=30, Κ=40) και μία "char symbol" ('c', 's', 'd', 'h').
     initializeDeck(&deck[0]);  //Δίνει τιμές στον πίνακα deck[] που αρχικά ήταν άδειος.
-    printDeck(&deck[0]);
-
-    separate();
-
     shuffleDeck(&deck[0]);
-    printDeck(&deck[0]);
+
+    gameloop(&deck[0]);
 
     return 0;
 }
