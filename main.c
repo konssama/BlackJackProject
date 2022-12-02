5ec1117e9893021d500b5f05a2595aa0457e4e10
konssama <samkostas1674@gmail.com>
2022-12-02 17:34:56 +0200

    changed main() structure

--- main.c
+++ main.c
@@ -12,12 +12,12 @@
 int main() {
     struct Card deck[52];      //Κάθε "Card" έχει μία τιμή "int value" (1-10 για τους αριθμούς ενώ οι φιγούρες στο εσωτερικά του προγράμματος συμβολίζονται J=20, Q=30, Κ=40) και μία "char symbol" ('c', 's', 'd', 'h').
     initializeDeck(&deck[0]);  //Δίνει τιμές στον πίνακα deck[] που αρχικά ήταν άδειος.
+    printDeck(&deck[0]);
 
-    printf("debug");
-
-    // separate();
+    separate();
 
     shuffleDeck(&deck[0]);
+    printDeck(&deck[0]);
 
     return 0;
 }
