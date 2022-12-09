91363f07a3fe66f8826a0ee143110c7f5f0ade96
konssama <samkostas1674@gmail.com>
2022-12-09 20:17:02 +0200

    updated valueOfCard()

--- drawCard.c
+++ drawCard.c
@@ -1,32 +1,40 @@
 
 // Επιστρέφει την πραγματική αξία μιας κάρτας.
-int valueOfCard(struct Card *currentCard, char mode) {
-    int value = 0;
+int valueOfCard(struct Card *currentCard) {
+    int realValue = 0;
 
     if ((*currentCard).value >= 2 && (*currentCard).value <= 10) {
-        value = (*currentCard).value;  // Εφόσον είναι αριθμός η τιμή του είναι απλα το value της κάρτας.
+        realValue = (*currentCard).value;  // Εφόσον είναι αριθμός η τιμή του είναι απλα το value της κάρτας.
 
     } else if ((*currentCard).value > 10) {
-        value = 10;  // Οι φιγούρες έχουν value 20, 30 και 40 αντίστοιχα. Στο παιχνίδι όμως όλες μετράνε για 10.
+        realValue = 10;  // Οι φιγούρες έχουν value 20, 30 και 40 αντίστοιχα. Στο παιχνίδι όμως όλες μετράνε για 10.
 
     } else if ((*currentCard).value == 1) {
-        int aceValue;
-
-        if (mode = 'c') {
-            printf("Picked an ace. You can either choose 1 or 10: ");  // Ο Άσος μετράει για 1 ή 10, ανάλογα με την επιλογή του παίκτη.
-
-            do {
-                scanf("%d", &aceValue);
-            } while (aceValue != 1 && aceValue != 10);
-
-        } else if (mode = 'r') {
-            //! ΠΡΕΠΕΙ ΚΑΠΩΣ ΝΑ ΔΙΑΛΕΞΟΥΜΕ ΤΥΧΑΙΑ 1 Η 10.
-            aceValue = 1;
-        }
-        value = aceValue;
+        realValue = -1;  // Στην περίπτωση του άσου επιστρέφουμε error -1 για να το διαχειριστεί άλλη συνάρτηση.
     }
 
-    return value;
+    return realValue;
+}
+
+int handleAce(struct Human *person) {
+    int aceValue;
+
+    if ((*person).name = 'p') {
+        printf("Picked an ace. You can either choose 1 or 10: ");  // Ο Άσος μετράει για 1 ή 10, ανάλογα με την επιλογή του παίκτη.
+
+        do {
+            scanf("%d", &aceValue);
+        } while (aceValue != 1 && aceValue != 10);
+
+    } else if ((*person).name = 'h') {
+        if ((*person).handValue + 10 > 21) {
+            aceValue = 1;
+        } else {
+            aceValue = 10;
+        }
+    }
+
+    return aceValue;
 }
 
 void drawCard(struct Human *person) {
@@ -37,12 +45,11 @@ void drawCard(struct Human *person) {
     (*person).nOfCards++;                                    // Οι παίκτης δημιουργεί μια νέα θέση στο χέρι του.
     (*person).currentHand[(*person).nOfCards] = pickedCard;  // Και βάζει σε αυτη τη θέση την κάρτα που τράβηξε.
 
-    char mode = '_';  // H valueOfCard πρέπει να έχει 2 modes για την περίπτωση του άσου.
-    if ((*person).name = 'p') {
-        mode = 'c';  // Στην περίπτωση που τράβηξε ο άνθρωπος θέλουμε να μπούμε στην valueOfCard με "choice" mode, για να μπορεί να επιλέξει την τιμή, στην περίπτωση που τράβηξε άσο.
-    } else if ((*person).name = 'h') {
-        mode = 'r';  // Αντίθετα, στην σειρά του οίκου μπαίνουμε με "random" mode για να επιλεχθεί η τιμή του άσου στην τύχη.
-    }
+    int pickedCardValue = valueOfCard(&pickedCard);  // Κρατάμε την πραγματική αξία που τράβηξε ο παίκτης.
 
-    (*person).handValue += valueOfCard(&pickedCard, mode);  // Μένει να προσθέσουμε την αξία αυτης της κάρτας στην συνολική.
+    if (pickedCardValue != -1) {
+        (*person).handValue += pickedCardValue;  // Εφόσον δεν έχει error -1 μπορούμε να προσθέσουμε την αξία στην συνολική.
+    } else {
+        (*person).handValue += handleAce(*person);  // Αλλιώς καλούμε ειδική συνάρτηση.
+    }
 }
